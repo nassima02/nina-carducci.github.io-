91b6623ce00b87d4df71fc7729aebaa35a3763c7
nassima bression <benasima@yahoo.fr>
2023-11-28 08:32:34 +0100

    refactoring

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,99 +1,148 @@
-const galleryItems = gallery.querySelectorAll(".gallery-item-clickable");
-const galleryItemsfilter = document.querySelectorAll('.gallery-item');
-// Sélectionnez tous les bouttons du filtre avec la classe "nav-link"
-const tagsList = document.querySelectorAll('.nav-link');
+const imagesGallery = document.querySelectorAll('.gallery-item');//sélectionner toutes les images de la gallery
+const filterButtonList = document.querySelectorAll('.nav-link');// Sélectionner tous les boutons du filtre
 
-let filteredGalleryImages = []; // TODO initialiser le tableau avec la liste des images au chargement de la page
-
+let filteredModalImages = [];
 let myModal = null;
 let isModalInit = false;
 
-galleryItems.forEach(function (item, index) {
-    item.addEventListener("click", function () {
+/**
+ * Initialise la galerie d'images pour la modal.
+ */
+function initializeGallery() {
+	filteredModalImages = [];
 
-        if (!isModalInit) {
-            isModalInit = true;
-            /*********************************************
-             *          INITIALISATION CARROUSEL
-             ********************************************/
+	imagesGallery.forEach(function (image) {
+		const nameCategoryImage = image.dataset.imageNameCategory;
 
-            const myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExampleControls'), {
-                interval: false // Désactive le défilement automatique
-            });
+		if (nameCategoryImage !== null) {
+			image.closest('.item-column').style.display = 'block';
+			filteredModalImages.push(image);
+		}
+	});
+}
 
-            // Ajout d'écouteurs d'événements pour les boutons de contrôle dans la modal
-            const nextButtonModal = document.querySelector("#carouselExampleControls .carousel-control-next");
-            nextButtonModal.addEventListener("click", function () {
-                myCarousel.next();
-            });
-
-            const prevButtonModal = document.querySelector("#carouselExampleControls .carousel-control-prev");
-            prevButtonModal.addEventListener("click", function () {
-                myCarousel.prev();
-            });
-
-            /*********************************************
-             *        INITIALISATION DE LA MODAL
-             ********************************************/
-
-            myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-
-            // l'ajout d'un écouteur d'événement pour fermer la modal en cliquant en dehors de celle-ci
-            const exampleModal = document.getElementById("exampleModal");
-            exampleModal.addEventListener('click', function (event) {
-                if (event.target === exampleModal) {
-                    myModal.hide();
-                }
-            });
-        }
-
-        const carouselInner = document.querySelector('#carouselExampleControls .carousel-inner')
-        carouselInner.innerHTML = "";
-
-        // l'ajout des images au carrousel
-        filteredGalleryImages.forEach(function (img, index) {
-            const carouselItem = document.createElement("div");
-            carouselItem.className = `carousel-item ${img === item ? 'active' : ''}`;
-            // carouselItem.className = "carousel-item active";
-            carouselItem.innerHTML = '<img src="' + img.src + '" class="d-block w-100" alt="..." loading="lazy">';
-            carouselInner.appendChild(carouselItem);
-        });
-
-        // Affiche la modal
-        myModal.show();
-    });
+// Appel de la fonction au chargement de la page
+window.addEventListener('load', function () {
+	initializeGallery();
 });
 
-// Sélectionne toutes les images de la galerie
-tagsList.forEach(function (tag) {
-    tag.addEventListener('click', function (event) {
-        // Récupère la valeur de l'attribut data-images-toggle (les noms des catégories du filtre)
-        const toggleValue = tag.getAttribute('data-images-toggle');
-        tagsList.forEach(function (t) {
-            t.classList.remove('active');
-            t.classList.remove('active-tag');
-        });
-        tag.classList.add('active');
-        tag.classList.add('active-tag');
-        // Parcourez toutes les images de la galerie
-        filteredGalleryImages = [];
+/**
+ * Ajoute un écouteur d'événement pour fermer la modal en cliquant en dehors de celle-ci.
+ */
+function closeModal() {
+	// l'ajout d'un écouteur d'événement pour fermer la modal en cliquant en dehors de celle-ci
+	const exampleModal = document.getElementById("exampleModal");
+	exampleModal.addEventListener('click', function (event) {
+		if (event.target === exampleModal) {
+			myModal.hide();
+		}
+	});
+}
 
-        galleryItemsfilter.forEach(function (item) {
-            //  data-gallery-tag est le nom de la catégorie de l'image
-            const galleryTag = item.getAttribute('data-gallery-tag');
+/**
+ *  Initialise la navigation de la modal avec la carousel Bootstrap.
+ */
+function navigationModal() {
+	const myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExampleControls'), {
+		interval: false // Désactive le défilement automatique
+	});
+	// Ajout d'écouteurs d'événements pour les boutons de contrôle dans la modal
+	const nextButtonModal = document.querySelector("#carouselExampleControls .carousel-control-next");
+	nextButtonModal.addEventListener("click", function () {
+		myCarousel.next();
+	});
 
-            if (galleryTag !== null) {
-                // Si la catégorie de l'image ne correspond pas à la catégorie sélectionnée
-                if (toggleValue !== 'all' && !galleryTag.includes(toggleValue)) {
-                    // Masquer l'image
-                    item.closest('.item-column').style.display = 'none';
-                } else {
-                    // Sinon, afficher la div parente
-                    item.closest('.item-column').style.display = 'block';
-                    filteredGalleryImages.push (item);
-                }
-            }
-        });
+	const prevButtonModal = document.querySelector("#carouselExampleControls .carousel-control-prev");
+	prevButtonModal.addEventListener("click", function () {
+		myCarousel.prev();
+	});
+}
 
-    });
-});
+/**
+ * Affiche la modal lorsqu'une image de la galerie est cliquée.
+ */
+function showImageModal() {
+	imagesGallery.forEach(function (image, index) {
+		image.addEventListener("click", function () {
+
+			initializeModal()
+
+			const carouselInner = document.querySelector('#carouselExampleControls .carousel-inner');
+			carouselInner.innerHTML = "";
+
+			filteredModalImages.forEach(function (img, index) {
+				const carouselItem = document.createElement("div");
+				carouselItem.className = `carousel-item ${img === image ? 'active' : ''}`;
+				// carouselItem.className = "carousel-item active";
+				carouselItem.innerHTML = '<img src="' + img.src + '" class="d-block w-100" alt="..." loading="lazy">';
+				carouselInner.appendChild(carouselItem);
+			});
+			myModal.show();
+		});
+	});
+}
+showImageModal();
+
+/**
+ *Initialise la modal si elle n'a pas déjà été initialisée.
+ */
+function initializeModal() {
+	if (!isModalInit) {
+		isModalInit = true;
+		navigationModal();
+		myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+		closeModal();
+	}
+}
+
+/**
+ * Ajoute un écouteur d'événement à chaque bouton de filtre du tableau filterButtonList.
+ * Supprime les classes «active» et «active-tag» de tous les boutons de filtre.
+ * Ajoute les classes «active» et «active-tag» au bouton de filtre cliqué.
+ * Efface le tableau filteredModalImages.
+ * Appelle la fonction imageModal avec le bouton de filtre cliqué comme argument.
+ *
+ * @return {void}
+ */
+function onClickButtonFilter() {
+	filterButtonList.forEach(function (filterButton) {
+		filterButton.addEventListener('click', function (event) {
+
+			filterButtonList.forEach(function (button) {
+				button.classList.remove('active');
+				button.classList.remove('active-tag');
+			});
+			filterButton.classList.add('active');
+			filterButton.classList.add('active-tag');
+
+			filteredModalImages = [];
+			imagesModal(filterButton);
+		});
+	});
+}
+onClickButtonFilter()
+
+/**
+ * filtre des images de la galerie en fonction du bouton de catégorie sélectionné.
+ * Masque ou affiche les images en fonction de leur catégorie.
+ *
+ * @param {HTMLElement} filterButton - Le bouton de catégorie utilisé pour le filtrage.
+ */
+function imagesModal(filterButton) {
+	imagesGallery.forEach(function (image) {
+
+		const nameCategoryImage = image.dataset.imageNameCategory;
+		const nameCategoryButton = filterButton.dataset.buttonNameCategory;
+		if (nameCategoryImage !== null) {
+			// Si la catégorie de l'image ne correspond pas à la catégorie du filtre sélectionné
+			if (nameCategoryButton !== 'all' && !nameCategoryImage.includes(nameCategoryButton)) {
+				// Masquer l'image (la div contenant l'image)
+				image.closest('.item-column').style.display = 'none'; //item-column est le nom de la div contenant l'image
+			} else {
+				// Sinon, afficher l'image (la div contenant l'image)
+				image.closest('.item-column').style.display = 'block';
+				filteredModalImages.push(image);
+			}
+		}
+	});
+}
